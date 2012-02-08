9db3bdf5e913e3d628631e9a095da06878120c81
Jean Connelly <jconnelly@cfa.harvard.edu>
2012-02-08 13:09:57 -0500

    Breaks out main into get_fov and fov_text()

--- fov.py
+++ fov.py
@@ -4,20 +4,38 @@ import re
 import os
 import getpass
 import urllib
+import logging
 from BeautifulSoup import BeautifulSoup
 
 import asciitable
 import numpy as np
 import Ska.DBI
-
 from Chandra.Time import DateTime
 
-import logging
-logger = logging.getLogger()
 
 last_aimpoint_shift = DateTime('2011:187')
+default_lts = 'http://cxc.harvard.edu/target_lists/longsched.html'
+logger = logging.getLogger()
+console = logging.StreamHandler()
+logger.setLevel(logging.INFO)
+logger.addHandler(console)
 
-def lts_table(lts_url='http://cxc.harvard.edu/target_lists/longsched.html'):
+
+
+
+def lts_table(lts_url=default_lts):
+    """ 
+    Grab the HTML long term schedule, skip everything except lines that
+    look like obsid lines, and cast them into a table using asciitable.
+
+    This does not get the lts week information available on that page from 
+    the paragraph order, but that can be retrieved directly from the 
+    axafocat.targets table
+    
+
+    """
+
+
     soup = BeautifulSoup(urllib.urlopen(lts_url).read())
     page = "".join(soup.findAll(text=True)) 
     pagelines = page.splitlines()
@@ -28,9 +46,9 @@ def lts_table(lts_url='http://cxc.harvard.edu/target_lists/longsched.html'):
         if re.match('^\d{6}\s+(P1)?\s+\d{1,5}.*\d{8}\s*$', line):
             obslines.append(line)
 
-    col_starts=(0, 7, 10, 16, 38, 45, 53, 61, 67, 
-                73, 80, 82, 84, 89, 110, 115, 
-                119, 122, 125, 128, 131, 134, 
+    col_starts=(0, 7, 10, 16, 38, 45, 53, 61, 67,
+                73, 80, 82, 84, 89, 110, 115,
+                119, 122, 125, 128, 131, 134,
                 138, 140, 165)
     col_ends=(6, 9, 14, 36, 43, 52, 60, 66, 72, 
               79, 81, 83, 88, 107, 112, 117, 
@@ -89,13 +107,99 @@ def default_offsets(det):
     return defaults[det]
 
 
-def main(reqids=[], outdir='.'):
-
-    console = logging.StreamHandler()
-    logger.setLevel(logging.INFO)
-    logger.addHandler(console)
+def get_fov(obsid, sqlsao, sqlaca):
 
     arcmin_mm = 2.925
+
+    target = sqlsao.fetchone("select * from target where obsid = %d" % obsid)        
+
+    if not target:
+        logger.warn("No entry for %d in target table" % obsid)
+        return None
+
+    roll = archive_roll(obsid, sqlaca)
+    if roll is None:
+        lts = lts_table()
+        roll = prelim_roll(obsid, lts)
+    if roll is None:
+        roll = 0
+
+    fov = dict(fovid="obs%05d" % target['obsid'],
+               groupid='',
+               grouping='None',
+               roll=roll,
+               coordinates="%(ra)f, %(dec)f" % target,
+               target=target['targname'],
+               offsetsimz=0.0,
+               offsetsimzmm=0.0,
+               grating=target['grating'],
+               offsety=target['y_det_offset'],
+               offsetz=target['z_det_offset'],
+               aca=0,
+               instrument=target['instrument'])
+
+    if (fov['offsety'] is None) or (fov['offsetz'] is None):
+        def_offsets = default_offsets(target['instrument'])
+        if fov['offsety'] is None:
+            fov['offsety'] = def_offsets[0]
+        if fov['offsetz'] is None:
+            fov['offsetz'] = def_offsets[1]
+
+    sim = sqlsao.fetchone("select * from sim where obsid = %d" % obsid)
+    if sim:
+    #    fov['offsetsimzmm'] = sim['trans_offset']
+        fov['offsetsimz'] = sim['trans_offset'] / arcmin_mm
+
+    if (target['acisid']):
+        fov['unselectedchips'] = 0
+        fov['subarrays'] = None
+        acis = sqlsao.fetchone("""select * from acisparam a, target t
+                               where a.acisid=t.acisid
+                               and a.acisid=%d""" % target['acisid'])
+        i_ccd_map = {'ccdi%d_on' % i : 'I%d' % i for i in range(0,4)}
+        s_ccd_map ={'ccds%d_on' % i : 'S%d' % i for i in range(0,6)}
+        ccd_map = dict(i_ccd_map.items() + s_ccd_map.items())
+        ccd_list = []
+        for x in ccd_map:
+            if acis[x] == 'Y' or re.match('O.+', acis[x]):
+                ccd_list.append('%s' % ccd_map[x])
+        fov['chips'] = ' '.join(ccd_list)
+        if acis['subarray'] != 'NONE':
+            fov['subarrays'] = \
+"""Custom
+      start row: %(subarray_start_row)s
+      number of rows: %(subarray_row_count)s""" % acis
+        fov['exposuremode'] = acis['exp_mode']
+        fov['nodeboundaries'] = 0
+
+    if (target['hrcid']):
+        fov['blankingmode'] = 'None'
+        fov['chipboundary'] = 1
+        hrc = sqlsao.fetchone("""select * from hrcparam h, target t
+                                 where h.hrcid=t.hrcid
+                                 and h.hrcid=%d""" % target['hrcid'])
+        if target['instrument'] == 'HRC-S':
+            fov['unselectedchips'] = 0
+            fov['timingmode'] = 0
+            if hrc['timing_mode'] == 'Y':
+                fov['timingmode'] = 1
+
+
+    if target['lts_lt_plan'] is not None:
+        ot = target['lts_lt_plan']
+        ot_date = DateTime('%04d:%03d' % (ot.year, ot.dayofyear))
+
+    if target['soe_st_sched_date'] is not None:
+        ot = target['soe_st_sched_date']
+        ot_date = DateTime('%04d:%03d' % (ot.year, ot.dayofyear))
+
+    if ot_date and ot_date.secs < last_aimpoint_shift.secs:
+        logger.warn("inaccurate fov: obsid %d predates last aimpoint shift." % obsid)
+
+    return fov
+
+
+def fov_text(fov):
     cols = ['groupid',
             'grouping',
             'coordinates',
@@ -117,6 +221,15 @@ def main(reqids=[], outdir='.'):
             'chipboundary',
             'timingmode']
 
+    fov_lines = []
+    fov_lines.append("fovid: %(fovid)s" % fov)
+    for col in cols:
+        if col in fov:
+            fov_lines.append("   %s: %s" % (col, fov[col]))
+    return fov_lines        
+    
+
+def main(reqids=[], outdir='.'):
 
     username = getpass.getuser()
     dbpassfile = os.path.join(os.environ['HOME'], '.arc5gl_pwd')
@@ -150,107 +263,31 @@ def main(reqids=[], outdir='.'):
 
     for obsid in obsids:
 
-        target = sqlsao.fetchone("select * from target where obsid = %d" % obsid)        
+        fov = get_fov(obsid, sqlsao, sqlaca)
 
-        roll = archive_roll(obsid, sqlaca)
-        if roll is None:
-            lts = lts_table()
-            roll = prelim_roll(obsid, lts)
-        if roll is None:
-            roll = 0
+        if not fov:
+            continue
 
-        fov = dict(fovid="obs%05d" % target['obsid'],
-                   groupid='',
-                   grouping='None',
-                   roll=roll,
-                   coordinates="%(ra)f, %(dec)f" % target,
-                   target=target['targname'],
-                   offsetsimz=0.0,
-                   offsetsimzmm=0.0,
-                   grating=target['grating'],
-                   offsety=target['y_det_offset'],
-                   offsetz=target['z_det_offset'],
-                   aca=0,
-                   instrument=target['instrument'])
-
-        if (fov['offsety'] is None) or (fov['offsetz'] is None):
-            def_offsets = default_offsets(target['instrument'])
-            if fov['offsety'] is None:
-                fov['offsety'] = def_offsets[0]
-            if fov['offsetz'] is None:
-                fov['offsetz'] = def_offsets[1]
-
-        sim = sqlsao.fetchone("select * from sim where obsid = %d" % obsid)
-        if sim:
-        #    fov['offsetsimzmm'] = sim['trans_offset']
-            fov['offsetsimz'] = sim['trans_offset'] / arcmin_mm
-
-        if (target['acisid']):
-            fov['unselectedchips'] = 0
-            fov['subarrays'] = None
-            acis = sqlsao.fetchone("""select * from acisparam a, target t
-                                   where a.acisid=t.acisid
-                                   and a.acisid=%d""" % target['acisid'])
-            i_ccd_map = {'ccdi%d_on' % i : 'I%d' % i for i in range(0,4)}
-            s_ccd_map ={'ccds%d_on' % i : 'S%d' % i for i in range(0,6)}
-            ccd_map = dict(i_ccd_map.items() + s_ccd_map.items())
-            ccd_list = []
-            for x in ccd_map:
-                if acis[x] == 'Y' or re.match('O.+', acis[x]):
-                    ccd_list.append('%s' % ccd_map[x])
-            fov['chips'] = ' '.join(ccd_list)
-            if acis['subarray'] != 'NONE':
-                fov['subarrays'] = \
-"""Custom
-      start row: %(subarray_start_row)s
-      number of rows: %(subarray_row_count)s""" % acis
-            fov['exposuremode'] = acis['exp_mode']
-            fov['nodeboundaries'] = 0
-
-        if (target['hrcid']):
-            fov['blankingmode'] = 'None'
-            fov['chipboundary'] = 1
-            hrc = sqlsao.fetchone("""select * from hrcparam h, target t
-                                     where h.hrcid=t.hrcid
-                                     and h.hrcid=%d""" % target['hrcid'])
-            if target['instrument'] == 'HRC-S':
-                fov['unselectedchips'] = 0
-                fov['timingmode'] = 0
-                if hrc['timing_mode'] == 'Y':
-                    fov['timingmode'] = 1
-        
-
-        if target['lts_lt_plan'] is not None:
-            ot = target['lts_lt_plan']
-            ot_date = DateTime('%04d:%03d' % (ot.year, ot.dayofyear))
-
-        if target['soe_st_sched_date'] is not None:
-            ot = target['soe_st_sched_date']
-            ot_date = DateTime('%04d:%03d' % (ot.year, ot.dayofyear))
-
-        if ot_date and ot_date.secs < last_aimpoint_shift.secs:
-            logger.warn("inaccurate fov: obsid %d predates last aimpoint shift." % obsid)
-                              
         if not os.path.exists(outdir):
             os.makedirs(outdir)
+
         outfile = os.path.join(outdir, 'obs%05d.fov' % obsid)
         fovfile = open(outfile, 'w')
-        fovfile.write("fovid: %(fovid)s\n" % fov)
-        for col in cols:
-            if col in fov:
-                fovfile.write("   %s: %s\n" % (col, fov[col]))
+        fov_lines = fov_text(fov)
+        fovfile.writelines('\n'.join(fov_lines))
         fovfile.close()
+
         logger.info("obsid %d fov written to: %s" % (obsid, outfile))
 
 
 def get_args():
     from argparse import ArgumentParser
     parser = ArgumentParser(description="obsvis fov generator from ocat")
-    parser.add_argument('reqids', 
+    parser.add_argument('reqids',
                         type=int,
                         nargs='+',
                         help="id or ids to fetch")
-    parser.add_argument("--outdir", 
+    parser.add_argument("--outdir",
                         default=".",
                         help="output directory for fov files")
     args = parser.parse_args()
